0e190b9cdd4fae7926ecb1b31791f190f3d8fbae
Milenium666 <whiteangel64rus@gmail.com>
2025-05-29 19:08:24 +0300

    fix: fixed review errors

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,29 @@
-
 import "./Header.css";
-
 import basket from "../../images/header-basket.svg";
 
 function Header() {
   return (
     <header className="header">
-      <div className="header__container">
-        <nav className="header__content">
-          <a className="header__logo-link" href="#">
-            <h1 className="header__logo">Sweet Paradise</h1>
-          </a>
-          <ul className="header__info">
-            <li className="header__info-list">О нас</li>
-            <li className="header__info-list">Каталог</li>
-            <li className="header__info-list">Отзывы</li>
-            <li className="header__info-list">Контакты</li>
-          </ul>
+      <nav className="header__content">
+        <h1 className="header__logo">
+          <a className="header__logo-link" href="/">Sweet Paradise</a>
+        </h1>
 
-          <a className="header__basket-link" href="#">
-            <img
-              src={basket}
-              alt="корзина для покупок"
-              className="header__basket"
-            />
-          </a>
-        </nav>
-      </div>
+        <ul className="header__info">
+          <li><a href="/about" className="header__info-link">О нас</a></li>
+          <li><a href="/catalog" className="header__info-link">Каталог</a></li>
+          <li><a href="/reviews" className="header__info-link">Отзывы</a></li>
+          <li><a href="/contacts" className="header__info-link">Контакты</a></li>
+        </ul>
+
+        <a className="header__basket-link" href="/cart" aria-label="Корзина">
+          <img
+            src={basket}
+            alt="Корзина для покупок"
+            className="header__basket"
+          />
+        </a>
+      </nav>
     </header>
   );
 }
