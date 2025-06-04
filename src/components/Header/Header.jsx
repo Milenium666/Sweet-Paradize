c2205fc688fc9a5057e21f139f18589735ad67b9
Milenium666 <whiteangel64rus@gmail.com>
2025-06-04 21:16:17 +0300

    feat: added the Paradise  label to the background

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,36 @@ import basket from "../../images/header-basket.svg";
 function Header() {
   return (
     <header className="header">
-              <div className="header_sweet">Sweet</div>
-
+      <div className="header_sweet">Sweet</div>
+      <div className="header__paradise">Paradise</div>
       <nav className="header__content">
         <h1 className="header__logo">
-          <a className="header__logo-link" href="/">Sweet Paradise</a>
+          <a className="header__logo-link" href="/">
+            Sweet Paradise
+          </a>
         </h1>
 
         <ul className="header__info">
-          <li><a href="/about" className="header__info-link">О нас</a></li>
-          <li><a href="/catalog" className="header__info-link">Каталог</a></li>
-          <li><a href="/reviews" className="header__info-link">Отзывы</a></li>
-          <li><a href="/contacts" className="header__info-link">Контакты</a></li>
+          <li>
+            <a href="/about" className="header__info-link">
+              О нас
+            </a>
+          </li>
+          <li>
+            <a href="/catalog" className="header__info-link">
+              Каталог
+            </a>
+          </li>
+          <li>
+            <a href="/reviews" className="header__info-link">
+              Отзывы
+            </a>
+          </li>
+          <li>
+            <a href="/contacts" className="header__info-link">
+              Контакты
+            </a>
+          </li>
         </ul>
 
         <a className="header__basket-link" href="/cart" aria-label="Корзина">
